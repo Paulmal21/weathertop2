fb6b19eb5669954fcbf89e1720a5f76a29830b3d
Glitch (weather-top-v2---web-dev-2---paul-maloney) <none>
2022-09-08 15:42:28 +0000

    🐧🚅 Checkpoint ./models/station-hub.js:48392717/1169 ./controllers/dashboard.js:48392717/1303

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,42 +1,17 @@
-"use strict";
+'use strict';
 
-const logger = require("../utils/logger");
-
-const stations = {
-  title: 'Dunmore',
-  readings: [
-    {
-      code: '100',
-      temp: '15',
-      windSpeed: '22',
-      windDirection: 'North',
-      pressure: '60',
-    },
-    {
-      code: '2100',
-      temp: '215',
-      windSpeed: '222',
-      windDirection: '2North',
-      pressure: '260',
-    },
-    {
-      code: '3100',
-      temp: '315',
-      windSpeed: '322',
-      windDirection: '3North',
-      pressure: '360',
-    },
-  ],
-};
+const logger = require('../utils/logger');
+const stations = require('../models/station-hub.js');
 
 const dashboard = {
   index(request, response) {
-    logger.info("dashboard rendering");
+    logger.info('dashboard rendering');
     const viewData = {
-      title: "WeatherTop Dashboard",
-      station: stations
+      title: 'Station Dashboard',
+      station: tramore,
     };
-    response.render("dashboard", viewData);
+    logger.info('about to render', stations);
+    response.render('dashboard', viewData);
   },
 };
 
---
+++ models/station-hub.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const logger = require('../utils/logger');
+
+const tramore = {
+  location: 'Tramore',
+  readings: [
+    {
+      code: '100',
+      temp: 'Beethoven',
+      windSpeed: 'Piano Sonata No. 3',
+      windDirection: 'Piano Sonata No. 3',
+      pressure: 'Piano Sonata No. 3',
+    },
+    {
+      code: 'Piano Sonata No. 3',
+      temp: 'Beethoven',
+      windSpeed: 'Piano Sonata No. 3',
+      windDirection: 'Piano Sonata No. 3',
+      pressure: 'Piano Sonata No. 3',
+    },
+    {
+      code: 'Piano Sonata No. 3',
+      temp: 'Beethoven',
+      windSpeed: 'Piano Sonata No. 3',
+      windDirection: 'Piano Sonata No. 3',
+      pressure: 'Piano Sonata No. 3',
+    },
+  ],
+};
+
+module.exports = tramore;
