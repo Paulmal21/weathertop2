3a2d210735ae2d3e896bbb20c66dee0eae35eba4
Glitch (weather-top-v2---web-dev-2---paul-maloney) <none>
2022-09-08 15:30:32 +0000

    💏👀 Checkpoint ./views/partials/listreadings.hbs:48392717/49 ./controllers/dashboard.js:48392717/887

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,11 +2,39 @@
 
 const logger = require("../utils/logger");
 
+const stations = {
+  title: 'Dunmore',
+  readings: [
+    {
+      code: '100',
+      temp: '15',
+      windSpeed: '22',
+      windDirection: 'North',
+      pressure: '60',
+    },
+    {
+      code: '2100',
+      temp: '215',
+      windSpeed: '222',
+      windDirection: '2North',
+      pressure: '260',
+    },
+    {
+      code: '3100',
+      temp: '315',
+      windSpeed: '322',
+      windDirection: '3North',
+      pressure: '360',
+    },
+  ],
+};
+
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "WeatherTop Dashboard",
+      station: stations
     };
     response.render("dashboard", viewData);
   },
