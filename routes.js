a56065e3919936a2ccc3c688f10d9a27cd2d689b
Glitch (weather-top-v2---web-dev-2---paul-maloney) <none>
2022-09-09 15:51:18 +0000

    🚎🛳 Checkpoint ./views/login.hbs:48392717/402 ./views/signup.hbs:48392717/725 ./controllers/accounts:48392717/1348 ./controllers/accounts.js:48392717/45 ./routes.js:48392717/447 ./views/partials/menu.hbs:48392717/60

--- routes.js
+++ routes.js
@@ -8,9 +8,9 @@ const router = express.Router();
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 
+const accounts = require('./controllers/accounts.js');
 
 
-router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
 
@@ -23,6 +23,13 @@ router.get('/dashboard/deletestation/:id', dashboard.deleteStation);
 router.post('/station/:id/addreading', station.addReading);
 
 router.post('/dashboard/addstation', dashboard.addStation);
+
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
 
 
 module.exports = router;
